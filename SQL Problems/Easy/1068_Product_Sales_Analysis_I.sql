65c3c7bb555f7e9f16d3ed4d579a56cd9d5c552c
Saranjeet Singh Saluja <58565610+saran820@users.noreply.github.com>
2022-02-28 13:04:44 -0500

    Add files via upload

---
+++ SQL Problems/Easy/1068_Product_Sales_Analysis_I.sql
@@ -0,0 +1,89 @@
+/*
+SQL Schema:
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+Write an SQL query that reports all product names of the products in the Sales table along with their selling year and price.
+
+For example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
+*/
+
+-- Table Creation Code:
+CREATE TABLE Sales (
+  `sale_id` INTEGER,
+  `product_id` INTEGER,
+  `year` INTEGER,
+  `quantity` INTEGER,
+  `price` INTEGER
+);
+
+INSERT INTO Sales
+  (`sale_id`, `product_id`, `year`, `quantity`, `price`)
+VALUES
+  ('1', '100', '2008', '10', '5000'),
+  ('2', '100', '2009', '12', '5000'),
+  ('7', '200', '2011', '15', '9000');
+
+CREATE TABLE Product (
+  `product_id` INTEGER,
+  `product_name` VARCHAR(7)
+);
+
+INSERT INTO Product
+  (`product_id`, `product_name`)
+VALUES
+  ('100', 'Nokia'),
+  ('200', 'Apple'),
+  ('300', 'Samsung');
+
+-- Solution/s:
+Select p.product_name, s.year, s.price
+From Product p
+Inner Join Sales s on (s.product_id = p.product_id)
