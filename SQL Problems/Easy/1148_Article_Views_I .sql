741abfda5d30b24888c50df896bbc703412752aa
Saranjeet Singh Saluja <58565610+saran820@users.noreply.github.com>
2022-03-04 13:03:05 -0500

    Add files via upload

---
+++ SQL Problems/Easy/1148_Article_Views_I .sql
@@ -0,0 +1,64 @@
+/*
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date.
+Note that equal author_id and viewer_id indicate the same person.
+Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.
+
+The query result format is in the following example:
+
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+
+Result table:
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+*/
+
+-- Table Creation Code:
+CREATE TABLE Views (
+  `article_id` INTEGER,
+  `author_id` INTEGER,
+  `viewer_id` INTEGER,
+  `view_date` DATETIME
+);
+
+INSERT INTO Views
+  (`article_id`, `author_id`, `viewer_id`, `view_date`)
+VALUES
+  ('1', '3', '5', '2019-08-01'),
+  ('1', '3', '6', '2019-08-02'),
+  ('2', '7', '7', '2019-08-01'),
+  ('2', '7', '6', '2019-08-02'),
+  ('4', '7', '1', '2019-07-22'),
+  ('3', '4', '4', '2019-07-21'),
+  ('3', '4', '4', '2019-07-21');
+
+-- Solution:
+Select distinct author_id as id
+From Views v
+Where v.author_id = v.viewer_id
+Order by 1 
