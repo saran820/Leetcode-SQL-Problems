71d159ef5ee067fbd75882babd4e01f02829c8d4
Saranjeet Singh Saluja <58565610+saran820@users.noreply.github.com>
2022-03-04 21:36:56 -0500

    Add files via upload

---
+++ SQL Problems/Easy/1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -0,0 +1,61 @@
+/*
+Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+Write a SQL query for a report that provides the pairs (actorid, directorid) where the actor have cooperated with the director at least 3 times.
+
+Example:
+
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+
+Result table:
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+The only pair is (1, 1) where they cooperated exactly 3 times.
+*/
+
+-- Table Creation Code:
+CREATE TABLE ActorDirector (
+  `actor_id` INTEGER,
+  `director_id` INTEGER,
+  `timestamp` INTEGER
+);
+
+INSERT INTO ActorDirector
+  (`actor_id`, `director_id`, `timestamp`)
+VALUES
+  ('1', '1', '0'),
+  ('1', '1', '1'),
+  ('1', '1', '2'),
+  ('1', '2', '3'),
+  ('1', '2', '4'),
+  ('2', '1', '5'),
+  ('2', '1', '6'),
+  ('2', '1', '7');
+
+-- Solution:
+Select actor_id, director_id
+From ActorDirector
+Group By actor_id, director_id
+Having count(1) > 2
