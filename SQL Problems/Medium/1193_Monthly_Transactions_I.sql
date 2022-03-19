2b9cb6a5abc915ea44352e916dfa74cc857c5633
Saranjeet Singh Saluja <58565610+saran820@users.noreply.github.com>
2022-03-19 15:06:41 -0400

    Used date_format, case statement and agg to get solution

---
+++ SQL Problems/Medium/1193_Monthly_Transactions_I.sql
@@ -0,0 +1,62 @@
+/*
+
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+The query result format is in the following example:
+
+Transactions table:
++------+---------+----------+--------+------------+
+| id   | country | state    | amount | trans_date |
++------+---------+----------+--------+------------+
+| 121  | US      | approved | 1000   | 2018-12-18 |
+| 122  | US      | declined | 2000   | 2018-12-19 |
+| 123  | US      | approved | 2000   | 2019-01-01 |
+| 124  | DE      | approved | 2000   | 2019-01-07 |
++------+---------+----------+--------+------------+
+
+Result table:
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+*/
+
+-- Table Creation Code:
+CREATE TABLE Transactions (
+  `id` INTEGER,
+  `country` VARCHAR(2),
+  `state` VARCHAR(8),
+  `amount` INTEGER,
+  `trans_date` DATETIME
+);
+
+INSERT INTO Transactions
+  (`id`, `country`, `state`, `amount`, `trans_date`)
+VALUES
+  ('121', 'US', 'approved', '1000', '2018-12-18'),
+  ('122', 'US', 'declined', '2000', '2018-12-19'),
+  ('123', 'US', 'approved', '2000', '2019-01-01'),
+  ('124', 'DE', 'approved', '2000', '2019-01-07');
+
+-- Solution:
+Select Date_format(trans_date,'%Y-%m') as month, country, count(id) as trans_count, sum(case when state='approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount
+From Transactions
+Group By month, country
