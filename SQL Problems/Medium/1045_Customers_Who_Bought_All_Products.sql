89f21c06bb444dbcc04fef7f603096cd0064bb32
Saranjeet Singh Saluja <58565610+saran820@users.noreply.github.com>
2022-04-02 16:26:54 -0400

    HAVING and COUNT used to get solution

---
+++ SQL Problems/Medium/1045_Customers_Who_Bought_All_Products.sql
@@ -0,0 +1,84 @@
+/*
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+product_key is a foreign key to Product table. Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key column for this table.
+Write an SQL query for a report that provides the customer ids from the Customer table that bought all the products in the Product table.
+
+For example:
+
+Customer table:
++-------------+-------------+
+| customer_id | product_key |
++-------------+-------------+
+| 1           | 5           |
+| 2           | 6           |
+| 3           | 5           |
+| 3           | 6           |
+| 1           | 6           |
++-------------+-------------+
+
+Product table:
++-------------+
+| product_key |
++-------------+
+| 5           |
+| 6           |
++-------------+
+
+Result table:
++-------------+
+| customer_id |
++-------------+
+| 1           |
+| 3           |
++-------------+
+The customers who bought all the products (5 and 6) are customers with id 1 and 3.
+
+*/
+
+--Table Creation Code:
+CREATE TABLE Customer (
+  `customer_id` INTEGER,
+  `product_key` INTEGER
+);
+
+INSERT INTO Customer
+  (`customer_id`, `product_key`)
+VALUES
+  ('1', '5'),
+  ('2', '6'),
+  ('3', '5'),
+  ('3', '6'),
+  ('1', '6');
+
+CREATE TABLE Product (
+  `product_key` INTEGER,
+  `ignore` VARCHAR(1)
+);
+
+INSERT INTO Product
+  (`product_key`)
+VALUES
+  ('5'),
+  ('6');
+
+-- Solution:
+Select c.customer_id
+From Customer c
+Group By c.customer_id
+Having count(distinct c.product_key) = (Select count(product_key)
+                               			From Product)
+
